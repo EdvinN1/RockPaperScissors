bf0fb42691fc683d8d0d6572c7e1ec8262ad9d0f
EdvinN1 <nedde14@gmail.com>
2022-12-12 11:45:12 +0100

    logic and redesign

--- main.js
+++ main.js
@@ -5,44 +5,76 @@ const scissors = 2
 let playerChoice
 let computerChoice
 
-let playerPoints 
-let computerPoints 
+let playerPoints = 0
+let computerPoints = 0
 
 let htmlPlayerPoints = document.getElementById("p1Score");
 let htmlComputerPoints = document.getElementById("p2Score");
+let winnerText = document.getElementById("winnerText");
+
 
 
 const choiceRock = document.getElementById("sten")
-choiceRock.addEventListener("click", function handleClick(){
+choiceRock.addEventListener("click", function handleClick() {
     playerChoice = 0;
     computerChoice = generateRandomNumber()
     checkWinner(computerChoice, playerChoice);
 })
-document.getElementById("sax")
-document.getElementById("pase")
+const choiceScissors = document.getElementById("sax")
+choiceScissors.addEventListener("click", function handleClick() {
+    playerChoice = 1;
+    computerChoice = generateRandomNumber()
+    checkWinner(computerChoice, playerChoice);
+})
 
-function checkWinner (computer, player) {
-if ((computer + 1) % 3 == player){
-    return playerPoints +1;
 
-} else if ((player + 1) % 3 == computer) {
-    return computerPoints +1;
-} else {
-    return console.log("DRAW")
+const choicePaper = document.getElementById("pase");
+choicePaper.addEventListener("click", function handleClick() {
+    playerChoice = 2;
+    computerChoice = generateRandomNumber()
+    checkWinner(computerChoice, playerChoice);
+})
+
+let winnerPageText = document.getElementById("resultOne");
+let winnerPageText2 = document.getElementById("resultTwo");
+
+function checkWinner(computer, player) {
+
+    if ((computer + 1) % 3 == player) {
+        playerPoints++
+        htmlPlayerPoints.innerText = playerPoints;
+        winnerText.innerText = "You got a point"
+    } else if ((player + 1) % 3 == computer) {
+        computerPoints++
+        htmlComputerPoints.innerText = computerPoints;
+        winnerText.innerText = "Opponent  got a point"
+    } else {
+        winnerText.innerText = "Draw"
+    }
+    if (playerPoints === 3) {
+        //  winnerText.innerText = "You won!"
+        window.location.href = "winnerPage.html";
+          winnerPageText.innerHtml = "You won!"
+          winnerPageText2.innerHtml = "You won!"
+          resetStats()
+
+  
+      } else if (computerPoints === 3) {
+         // winnerText.innerText = "You lost!"
+          window.location.href = "winnerPage.html";
+          winnerPageText.innerHtml = "You lost!"
+          winnerPageText2.innerHtml = "You lost!"
+          resetStats()
+          
+      }
 }
-}
+//setTimeout(function () { resetStats() }, 3000);
 function generateRandomNumber() {
-   return Math.floor(Math.random() * 3);
+    return Math.floor(Math.random() * 3);
 }
-if(playerPoints === undefined){
-    htmlPlayerPoints.innerText = 0
-} else {
-    htmlPlayerPoints.innerText = playerPoints
+function resetStats() {
+    htmlPlayerPoints.innerText = 0;
+    htmlComputerPoints.innerText = 0;
+    playerPoints = 0
+    computerPoints = 0
 }
-if(computerPoints  === undefined){
-    htmlComputerPoints.innerText = 0
-} else {
-    htmlComputerPoints.innerText = computerPoints
-}
-
-console.log(playerPoints /* computerPoints*/)
